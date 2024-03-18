ff4c2d2cb195716a2e1ca739d77e71453819447b
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-03-18 19:54:39 +0300

    feat: add connection config

---
+++ src/main/java/app/banking/configuration/Connection.java
@@ -0,0 +1,16 @@
+package app.banking.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import postgres.addict.Pool;
+
+@Configuration
+public class Connection {
+  @Bean
+  public Pool poolConnection(){
+    String url = System.getenv("DATABASE_URL");
+    String user = System.getenv("DATABASE_USERNAME");
+    String password = System.getenv("DATABASE_PASSWORD");
+    return Pool.create(url, user, password);
+  }
+}
