0990b8ca4a5a91fc019ba00c67d23c8bf550ada3
Abi-Nf <hei.abigail.3@gmail.com>
2024-03-23 15:07:28 -0400

    feat: Common repository

---
+++ src/main/java/postgres/addict/CommonRepository.java
@@ -0,0 +1,51 @@
+package postgres.addict;
+
+import lombok.SneakyThrows;
+
+import java.sql.PreparedStatement;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+public class CommonRepository<T, R> extends AddictRepository<T> {
+  @SneakyThrows
+  public Optional<T> findById(R id){
+    PreparedStatement statement = createStatement("SELECT * FROM @table WHERE @id = ?");
+    statement.setObject(1, id);
+    return resultSetOptional(statement);
+  }
+
+  public List<T> findAll(){
+    return listOfStatement("SELECT * FROM @table");
+  }
+
+  @SneakyThrows
+  public Optional<T> updateById(R id, T newValue){
+    HashMap<String, Object> values = getColumnValues(newValue);
+    String params = String.join(",", values.keySet()
+      .stream().map(key -> key + " = ?").toList()
+    );
+    String query = "UPDATE @table SET @params WHERE @id = ? RETURNING *"
+      .replace("@params", params);
+    int valueSize = values.size();
+    PreparedStatement statement = createStatement(query);
+    List<Object> objectValues = values.values().stream().toList();
+    for (int i = 0; i < objectValues.size(); i++) {
+      Object value = objectValues.get(i);
+      if(value instanceof Enum<?>){
+        statement.setObject(i+1, ((Enum<?>) value).name());
+      }else {
+        statement.setObject(i+1, value);
+      }
+    }
+    statement.setObject(valueSize+1, id);
+    return resultSetOptional(statement);
+  }
+
+  @SneakyThrows
+  public Optional<T> deleteById(R id){
+    PreparedStatement statement = createStatement("DELETE FROM @table WHERE @id = ? RETURNING *");
+    statement.setObject(1, id);
+    return resultSetOptional(statement);
+  }
+}
