5a0cf49b25c08fa02b2af2a0fbb14e0971dde907
Abi-Nf <hei.abigail.3@gmail.com>
2024-03-24 14:17:00 -0400

    feat: find by name & type

--- src/main/java/app/banking/repository/CategoryRepository.java
+++ src/main/java/app/banking/repository/CategoryRepository.java
@@ -1,7 +1,19 @@
 package app.banking.repository;
 
 import app.banking.models.Category;
+import app.banking.models.TransactionType;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+
 @Repository
-public class CategoryRepository extends CommonCrud<Category, Long> {}
+public class CategoryRepository extends CommonCrud<Category, Long> {
+  @SneakyThrows
+  public Category findByNameAndType(String name, TransactionType type) {
+    PreparedStatement statement = createStatement("SELECT name FROM @table WHERE name = ? AND only_on = ?");
+    statement.setString(1, name);
+    statement.setString(2, type.name());
+    return forceResultSet(statement);
+  }
+}
