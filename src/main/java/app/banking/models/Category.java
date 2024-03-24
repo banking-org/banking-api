6cca0a0e310b1fac8b174d695951c0720797fcc7
Abi-Nf <hei.abigail.3@gmail.com>
2024-03-24 14:15:28 -0400

    feat: builder

--- src/main/java/app/banking/models/Category.java
+++ src/main/java/app/banking/models/Category.java
@@ -1,8 +1,6 @@
 package app.banking.models;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import lombok.*;
 import postgres.addict.Column;
 import postgres.addict.ColumnType;
 import postgres.addict.GenerativeValue;
@@ -10,13 +8,16 @@ import postgres.addict.Table;
 
 @Data
 @ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode
 @Table
 public class Category {
   @Column(identity=true, generative= GenerativeValue.SEQUENCE)
   private Long id;
 
-  @Column(required=true)
+  @Column(required=true, unique=true)
   private String name;
 
   @Column(columnType=ColumnType.TEXT)
