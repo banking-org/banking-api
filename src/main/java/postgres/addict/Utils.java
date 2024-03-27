cddba0cf49ff999f06de9e54e49f14623ea34fe3
Abi-Nf <hei.abigail.3@gmail.com>
2024-03-27 12:06:30 -0400

    feat: object mapper

--- src/main/java/postgres/addict/Utils.java
+++ src/main/java/postgres/addict/Utils.java
@@ -1,7 +1,35 @@
 package postgres.addict;
 
+import java.sql.*;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Utils {
   public static String spaceToSnakeCase(String value){
     return value.trim().replace(" ", "_");
+  }
+
+  public static String objectToPsqlValue(Object value){
+    if(value instanceof String || value instanceof Character) {
+      return "'" + value + "'";
+    }
+    if(value instanceof Enum<?>){
+      return "'" + ((Enum<?>) value).name() + "'";
+    }
+    if(value instanceof LocalDate){
+      return "'" + Date.valueOf((LocalDate) value) + "'::date";
+    }
+    if(value instanceof Instant) {
+      return "'" + Timestamp.from((Instant) value) + "'::timestamp";
+    }
+    if(value instanceof LocalDateTime){
+      return "'" + Timestamp.valueOf((LocalDateTime) value) + "'::timestamp";
+    }
+    if(value instanceof LocalTime){
+      return "'" + Time.valueOf((LocalTime) value) + "'::time";
+    }
+    return value.toString();
   }
 }
