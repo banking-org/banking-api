ffc98f158feeba439793482f2c23bc3683004d2a
Abi-Nf <hei.abigail.3@gmail.com>
2024-03-28 19:50:24 -0400

    feat: bind mutations

--- src/main/java/postgres/addict/migrations/Migration.java
+++ src/main/java/postgres/addict/migrations/Migration.java
@@ -2,6 +2,7 @@ package postgres.addict.migrations;
 
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import postgres.addict.Pool;
 import postgres.addict.runtime.definition.ColumnDefinition;
 import postgres.addict.runtime.definition.TableDefinition;
@@ -11,39 +12,60 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.*;
 
+@Slf4j
 @RequiredArgsConstructor
 public class Migration {
   private final String path;
   private final Pool pool;
 
+  private boolean checkMutation = false;
+
+  public void mutate(boolean value){
+    this.checkMutation = value;
+  }
+
   @SneakyThrows
   public void init(Class<?>... entities){
     Connection connection = pool.getConnection();
+    ResultSet tableSet = getTableSet(connection);
 
     for (Class<?> entity : entities) {
       TableDefinition<?> tableDefinition = new TableDefinition<>(entity);
-      String tableName = tableDefinition.getName();
       tableDefinition.getColumns()
         .stream()
         .filter(ColumnDefinition::isReferences)
-        .forEach(columnDefinition -> {
-          Class<?> clazz = columnDefinition.getRefTableDefinition().getClazz();
+        .forEach(def -> {
+          Class<?> clazz = def.getRefTableDefinition().getClazz();
           new Migration(path, pool).init(clazz);
         });
 
-      try  {
-
-        String sqlCreateTable = new MigrationQuery<>(entity).createTable();
+      String tableName = tableDefinition.getName();
+      boolean tableExists = false;
+      if(tableSet.next()){
+        String schema = tableDefinition.getSchema();
+        if(
+          tableName.equals(tableSet.getString("TABLE_NAME")) &&
+          schema.equals(tableSet.getString("TABLE_SCHEM"))
+        ){
+          tableExists = true;
+        }
+      }
+      if(!tableExists){
+        String sqlCreateTable = new MigrationQuery<>(tableDefinition).createTable();
         Statement statement = connection.createStatement();
         statement.execute(sqlCreateTable);
         saveQueryToFile(sqlCreateTable, tableName + "_v1.sql");
-      }finally {
-        pool.releaseConnection(connection);
+      }else {
+        if(checkMutation){
+          log.info("Check schema mutations...");
+          Mutation.check(tableDefinition);
+        }
       }
     }
   }
 
   @SneakyThrows
+  @SuppressWarnings("all")
   public void readAllIn(String subPath){
     Path root = Path.of(this.path, subPath);
     if (Files.exists(root)) {
@@ -52,6 +74,7 @@ public class Migration {
   }
 
   @SneakyThrows
+  @SuppressWarnings("all")
   public void readFile(Path path){
     Path src = path.toAbsolutePath();
     File file = new File(src.toUri());
@@ -79,5 +102,17 @@ public class Migration {
     FileWriter writer = new FileWriter(file);
     writer.write(sql);
     writer.close();
+  }
+
+  @SneakyThrows
+  private ResultSet getTableSet(Connection connection){
+    DatabaseMetaData metadata = connection.getMetaData();
+    return metadata
+      .getTables(
+        null,
+        null,
+        null,
+        new String[]{ "TABLE" }
+      );
   }
 }
