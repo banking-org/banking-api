b8d42351a4aea0e73bd3da4cb6da8ab2111ac8d1
Abi-Nf <hei.abigail.3@gmail.com>
2024-03-28 19:48:16 -0400

    feat: refactor

--- src/main/java/postgres/addict/migrations/MigrationQuery.java
+++ src/main/java/postgres/addict/migrations/MigrationQuery.java
@@ -1,81 +1,73 @@
 package postgres.addict.migrations;
 
 import lombok.SneakyThrows;
-import postgres.addict.ColumnType;
 import postgres.addict.GenerativeValue;
-import postgres.addict.TypeMapper;
 import postgres.addict.runtime.definition.ColumnDefinition;
 import postgres.addict.runtime.definition.TableDefinition;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static postgres.addict.TypeMapper.typeLimiter;
+
 public class MigrationQuery<T> {
   private final String tableName;
-  private final List<ColumnDefinition> columns = new ArrayList<>();
+  private final List<ColumnDefinition> columns;
 
   public MigrationQuery(TableDefinition<T> table){
     this.tableName = table.getName();
-    this.columns.add(table.getColumnIdentity());
-    this.columns.addAll(table.getColumns());
+    this.columns = table.getColumns();
   }
 
   @SneakyThrows
-  @SuppressWarnings("unchecked")
   public MigrationQuery(Class<?> entity){
-    TableDefinition<T> table = (TableDefinition<T>) new TableDefinition<>(entity);
+    TableDefinition<?> table = new TableDefinition<>(entity);
     this.tableName = table.getName();
-    this.columns.add(table.getColumnIdentity());
-    this.columns.addAll(table.getColumns());
+    this.columns = table.getColumns();
+  }
+
+  public String dropTable(){
+    return "DROP TABLE IF EXISTS \"" + tableName + "\" CASCADE";
   }
 
   public String createTable(){
     List<String> columns = new ArrayList<>();
     for (ColumnDefinition column: this.columns){
       columns.add(
-          this.lineColumnOnCreate(column)
+        this.lineColumnOnCreate(column)
       );
     }
-    return "CREATE TABLE IF NOT EXISTS \"" + this.tableName + "\" (\n" + String.join(",\n", columns) + "\n);\n";
-  }
-
-  public String dropTable(){
-    return "DROP TABLE IF EXISTS \"" + this.tableName + "\" CASCADE";
+    return "CREATE TABLE IF NOT EXISTS \"" +
+      tableName +
+      "\" (\n" +
+      String.join(",\n", columns) +
+      "\n);\n";
   }
 
   private String lineColumnOnCreate(ColumnDefinition column){
-    String columnName = column.getName();
-    String bruteType = column.getColumnType();
-    String baseType =
-        (column.getGenerative() == GenerativeValue.SEQUENCE)
-            ? TypeMapper.typeToSequenceIfPossible(bruteType)
-            : bruteType;
-
-    return "  \"" + columnName + "\" " +
-        typeLimiter(column.isReferences() ? bruteType : baseType, column) +
-        (column.isRequired() ? " NOT NULL" : "") +
-        (column.isUnique() ? " UNIQUE" : "") +
-        (
-            !column.getDefaultValue().isEmpty()
-                ? (" DEFAULT " + column.getDefaultValue())
-                : (column.getGenerative() == GenerativeValue.UUID ? " DEFAULT gen_random_uuid()" : "")
-        ) +
-        (column.isIdentity() ? " PRIMARY KEY" : "") +
-        (
-            column.isReferences() ? " REFERENCES \"" +
-            column.getRefTableDefinition().getName() + "\"(" +
-            column.getRefTableDefinition().getColumnIdentity().getName() + ") ON DELETE CASCADE" : ""
-        );
+    return "  \"" + column.getName() + "\" " + typeLimiter(column) + (column.isRequired() ? " NOT NULL" : "") + commonAddColumn(column);
   }
 
-  public static String typeLimiter(String type, ColumnDefinition definition){
-    return switch (type){
-      case ColumnType.VARCHAR,
-          ColumnType.BPCHAR,
-          ColumnType.CHAR,
-          ColumnType.BIT -> type + "(" + definition.getSize() + ")";
-      case ColumnType.NUMERIC -> type + "(" + definition.getPrecision() + ", " + definition.getScale() + ")";
-      default -> type;
-    };
+  public static String commonAddColumn(ColumnDefinition column){
+    StringBuilder builder = new StringBuilder(column.isUnique() ? " UNIQUE" : "");
+    if(!column.getDefaultValue().isEmpty()){
+      builder
+        .append(" DEFAULT ")
+        .append(column.getDefaultValue());
+    }else if(column.getGenerative() == GenerativeValue.UUID){
+      builder.append(" DEFAULT gen_random_uuid()");
+    }
+    if(column.isIdentity()){
+      builder.append(" PRIMARY KEY");
+    }
+    if(column.isReferences()){
+      builder
+        .append(" REFERENCES \"")
+        .append(column.getRefTableDefinition().getName())
+        .append("\"(")
+        .append(column.getRefTableDefinition().getColumnIdentity().getName())
+        .append(") ON DELETE CASCADE");
+    }
+    return builder.toString();
   }
 }
