50213cd0ac8e022511098ea36a55a9ee01fbcf9d
Abi-Nf <hei.abigail.3@gmail.com>
2024-03-28 19:47:06 -0400

    feat: type limiter for varying

--- src/main/java/postgres/addict/TypeMapper.java
+++ src/main/java/postgres/addict/TypeMapper.java
@@ -1,5 +1,6 @@
 package postgres.addict;
 
+import postgres.addict.runtime.definition.ColumnDefinition;
 
 public class TypeMapper {
   public static final String NO_TYPE_MAPPED = "text";
@@ -25,6 +26,23 @@ public class TypeMapper {
       case ColumnType.INT2 -> "smallserial";
       case ColumnType.BIGINT, ColumnType.INT8 -> "bigserial";
       case ColumnType.INTEGER, ColumnType.INT4 -> "serial";
+      default -> type;
+    };
+  }
+
+  public static String typeLimiter(ColumnDefinition definition){
+    String bruteType = definition.getColumnType();
+    String baseType =
+      (definition.getGenerative() == GenerativeValue.SEQUENCE)
+        ? TypeMapper.typeToSequenceIfPossible(bruteType)
+        : bruteType;
+    String type = definition.isReferences() ? bruteType : baseType;
+    return switch (type){
+      case ColumnType.VARCHAR,
+        ColumnType.BPCHAR,
+        ColumnType.CHAR,
+        ColumnType.BIT -> type + "(" + definition.getSize() + ")";
+      case ColumnType.NUMERIC -> type + "(" + definition.getPrecision() + ", " + definition.getScale() + ")";
       default -> type;
     };
   }
